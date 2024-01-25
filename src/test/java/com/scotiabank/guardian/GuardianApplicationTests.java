fd270218b40b4c11e4348e7dc7072858fe9775b7
Ludwing Badillo <ludwingbadillo@192.168.0.4>
2024-01-25 10:12:22 -0500

    create

---
+++ src/test/java/com/scotiabank/guardian/GuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scotiabank.guardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
